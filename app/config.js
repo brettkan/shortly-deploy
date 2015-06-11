4fd9ff40d6701753b29b29829cbdead4d08bd0d5
Hack Reactor Students <accounts@hackreactor.com>
2015-06-11 14:59:57 -0700

    Added additional error handling

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-var connectionUrl = process.MONGO || 'mongodb://localhost:27017/chat';
+var connectionUrl = process.env.CUSTOMCONNSTR_MONGOLAB_URI || 'mongodb://localhost:27017/chat';
 
 var db = mongoose.connect(connectionUrl, function(err) {
   if (err) {
